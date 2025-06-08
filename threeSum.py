78234f801e469b1bfdaf516d54bbef8ec16b248f
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-06-07 23:53:22 -0500

    Update threeSum.py

--- threeSum.py
+++ threeSum.py
@@ -1,1 +1,28 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        nums.sort()
+        res = []
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                threesum = nums[i] + nums[l] + nums[r]
+                if threesum < 0:
+                    l += 1
+                elif threesum > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+        return res
+
 
